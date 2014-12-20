78a68de8143cdd4d390c72772e78055997c232f3
Brian MacAllister <bmacallister85@gmail.com>
2014-12-20 18:22:36 -0500

    camera_node: commented

--- camera_node/include/camera_node/camera_node.h
+++ camera_node/include/camera_node/camera_node.h
@@ -14,33 +14,44 @@
 #include <camera_node/camera_scan.h>
 #include <sensor_msgs/Image.h>
 
-
+/**
+ * \brief ros node for retrieving camera images and publishing corresponding camera scans
+ */
 class camera_scan_node
 {
 
 public:
 
 	/**
-	 *
+	 * \brief constructor
 	 */
 	camera_scan_node();
 
 
 	/**
-	 *
+	 * \brief destructor
 	 */
 	~camera_scan_node();
 
+
 	/**
-	 *
+	 * \brief configures ros related entities including params and callback functions
 	 */
 	void ros_configure();
 
 
 private:
 
+	/**
+	 * \brief callback function when image of camera received
+	 * @param msg the msg the image is in
+	 */
 	void camera_image_callback(const sensor_msgs::ImageConstPtr msg);
 
+	/**
+	 * \brief publishes the camera scan with same timestamp as it coresponding image
+	 * @param scan the camera scan
+	 */
 	void publish_camera_scan(const camera_node::camera_scan & scan);
 
 	//ros related
@@ -53,9 +64,9 @@ private:
 	std::string image_topic_name;
 
 	//scan related
-	double sensor_field_of_view_rad;
-	double sensor_distance;
-	double sensor_angular_resolution;
+	double sensor_field_of_view_rad; /** angle (in radians) of sensor field of view */
+	double sensor_distance; /** max distance of scan */
+	double sensor_angular_resolution; /**angular distance between rays cast is scan */
 
 };
 
